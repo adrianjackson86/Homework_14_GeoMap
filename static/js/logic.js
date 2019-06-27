d1ac0fc6281c58ef1aa0d793c9a8f6eab3d38def
AJ <AJ@users.noreply.github.com>
2019-06-27 16:07:34 -0400

    json working, map working, still no circles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,10 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson" 
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson
+
+// "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+//  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -58,9 +62,9 @@ function createMap(earthquakes) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      39.8283, -98.5795
     ],
-    zoom: 5,
+    zoom: 3,
     layers: [streetmap, earthquakes]
   });
 
